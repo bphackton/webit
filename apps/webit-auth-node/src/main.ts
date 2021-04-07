110ceeb82fbde90eff5335b6bea2592fa5415d83
ruslan10 <rusik85@gmail.com>
2021-04-07 17:20:09 +0300

    add websocket

--- apps/webit-auth-node/src/main.ts
+++ apps/webit-auth-node/src/main.ts
@@ -1,18 +1,30 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  path: '/ws',
+  transports: ['websocket']
+});
+
+io.on('connection', (socket: Socket) => {
+  socket.on('event', (data) => {
+    console.log(data);
+  });
+  socket.on('disconnect', (reason) => {
+    console.log(reason);
+  });
+});
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to webit-auth-node!' });
 });
 
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
+httpServer.listen(port,() => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+
+httpServer.on('error', console.error);
