5b4130a528d19d910af4cc9444bf3119b3b96335
danielle <daniellezra1@gmail.com>
2021-04-25 21:59:20 +0300

    add component and services

---
+++ apps/webit-client/src/app/components/webit-movments-summary/webit-movments-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'webit-movements-summary',
+  templateUrl: './webit-movements-summary.component.html',
+  styleUrls: ['./webit-movements-summary.component.scss']
+})
+export class WebitMovementsSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
