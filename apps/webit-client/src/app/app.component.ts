89f657214d308efb2689e57f818c5f8814f6ff6a
Ruslan <rusik85@gmail.com>
2021-05-02 12:25:11 +0300

    initial commit

---
+++ apps/webit-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'webit-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'webit-client';
+}
