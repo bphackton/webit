f6940002758c9372683d4601068b43ff22ea3f2a
ruslan10 <rusik85@gmail.com>
2021-05-12 13:53:28 +0300

    fix ws bug

--- apps/webit-client/src/app/cache.service.ts
+++ apps/webit-client/src/app/cache.service.ts
@@ -1,10 +1,8 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CacheService {
   storage: Storage = window.sessionStorage;
-
-  constructor() {}
 
   get(key: string): any {
     const strVal = this.storage.getItem(key);
@@ -14,7 +12,7 @@ export class CacheService {
   set(key: string, value: any, ttl = 0): string {
     if (!key) {
       // key cannot be falsy, to make retrievals safe
-      throw new Error("CacheService.set(): invalid/null key: " + String(key));
+      throw new Error('CacheService.set(): invalid/null key: ' + String(key));
     }
     const strVal = JSON.stringify(value);
     this.storage.setItem(key, strVal);
